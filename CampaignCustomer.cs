26e19eac7960c832549bb812f1ab923b2a6880e0
bansalrahul1 <bansalrahul1@gmail.com>
2023-09-27 17:51:57 +0530

    Add files via upload

---
+++ CampaignCustomer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Models
+{
+    public class CampaignCustomer
+    {
+        public int Campaign_Id { get; set; }
+        public int Customer_Id { get; set; }
+        public string Customer_Name { get; set; }
+        public string CustomerTable { get; set; }
+        public string Customer_State { get; set; }
+        public string Product { get; set; }
+        public string Decile { get; set; }
+        public string Tenure { get; set; }
+        public int TargetVolume { get; set; }
+
+    }
+}
